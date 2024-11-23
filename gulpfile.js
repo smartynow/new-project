f3dbf2287ea1a3afddc260cb2e18ba405e102700
Sergei Martynov <sergei.mmssaa@gmail.com>
2024-11-23 09:51:57 +0200

    Update gulpfile.js
    Add watcher on all files

--- gulpfile.js
+++ gulpfile.js
@@ -10,55 +10,90 @@ import rename from "gulp-rename";
 import { deleteAsync } from "del";
 
 const path = {
-    build: {
-        css: "assets/css/",
-    },
-    src: {
-        scss: `assets/scss/**/*.scss`,
-    },
-    watch: {
-        scss: `assets/scss/**/*.scss`,
-    },
+  build: {
+    css: "assets/css/",
+  },
+  src: {
+    html: "*.html",
+    scss: `assets/scss/**/*.scss`,
+    js: "assets/js/app.js",
+    fonts: "assets/fonts/*.*",
+    img: "assets/img/*.*",
+  },
+  watch: {
+    html: "*.html",
+    css: "assets/css/*.css",
+    scss: `assets/scss/**/*.scss`,
+    js: "assets/js/**/*.js",
+    fonts: "assets/fonts/**/*.*",
+    img: "assets/img/**/*.*",
+  },
 };
 
 // Tasks
 //Reset
 const reset = () => {
-    return deleteAsync(['assets/css/']);
+  return deleteAsync(["assets/css/"]);
 };
 
 // Compile SCSS
 const scss = () => {
-    return gulp
-        .src(path.src.scss, { sourcemaps: true })
-        .pipe(
-            sass({
-                outputStyle: "expanded",
-            }).on('error', sass.logError)
-        )
-        .pipe(
-            autoPrefixer({
-                grid: true,
-            })
-        )
-        .pipe(postcss([cssnano()]))
-        .pipe(rename({ extname: ".min.css" }))
-        .pipe(gulp.dest(path.build.css))
-        .pipe(browserSync.stream());
+  return gulp
+    .src(path.src.scss, { sourcemaps: true })
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      }).on("error", sass.logError),
+    )
+    .pipe(
+      autoPrefixer({
+        grid: true,
+      }),
+    )
+    .pipe(postcss([cssnano()]))
+    .pipe(rename({ extname: ".min.css" }))
+    .pipe(gulp.dest(path.build.css))
+    .pipe(browserSync.stream());
 };
 
 // Local Server
 const server = (done) => {
-    browserSync.init({
-        server: { baseDir: "./" },
-        port: 3000,
-    });
-    done();
+  browserSync.init({
+    server: { baseDir: "./" },
+    port: 3000,
+    notify: false, // Отключаем уведомления
+    open: true, // Автоматически открывать браузер
+    cors: true,
+    ui: false,
+  });
+  done();
+};
+
+// Reload Server
+const reload = (done) => {
+  browserSync.reload();
+  done();
 };
 
 // Watcher
 const watcher = () => {
-    gulp.watch(path.watch.scss, scss);
+  // Watch SCSS
+  gulp.watch(path.watch.scss, scss);
+
+  // Watch HTML and reload on changes
+  gulp.watch(path.watch.html).on("change", browserSync.reload);
+
+  // Watch CSS and reload on changes
+  gulp.watch(path.watch.css).on("change", browserSync.reload);
+
+  // Watch JS and reload on changes
+  gulp.watch(path.watch.js).on("change", browserSync.reload);
+
+  // Watch fonts and reload on changes
+  gulp.watch(path.watch.fonts).on("change", browserSync.reload);
+
+  // Watch images and reload on changes
+  gulp.watch(path.watch.img).on("change", browserSync.reload);
 };
 
 // Main Tasks
